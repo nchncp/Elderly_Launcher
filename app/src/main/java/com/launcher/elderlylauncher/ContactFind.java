2367478880880e8137473bab261273042fa74964
Nichapa P <nchncp@gmail.com>
2016-11-05 19:25:29 +0700

    Finish Add Favourite Contact

--- app/src/main/java/com/launcher/elderlylauncher/ContactFind.java
+++ app/src/main/java/com/launcher/elderlylauncher/ContactFind.java
@@ -1,25 +1,96 @@
 package com.launcher.elderlylauncher;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class ContactFind extends Activity {
 
+    private EditText searchUser;
     private Button search;
+    private ListView listContact;
+
+    private String[] cList;
+
+    private ContactBook[] listContactBook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_find);
 
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        searchUser = (EditText)findViewById(R.id.searchContact);
+
         search = (Button)findViewById(R.id.btnSearch);
+
+        listContact = (ListView)findViewById(R.id.listContact);
+        listContact.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SharedPreferences sharedPreferences = getSharedPreferences("PREF_NAME", Context.MODE_PRIVATE);
+                int numContact = sharedPreferences.getInt("numContact", 0);
+                numContact++;
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                Set<String> newSet = new HashSet<String>();
+                newSet.add(null);
+                newSet.add("user:"+listContactBook[position].getmUsername());
+                newSet.add("name:"+listContactBook[position].getmName());
+                newSet.add("lname:"+listContactBook[position].getmLName());
+                newSet.add("phone:"+listContactBook[position].getmPhone());
+                newSet.add("email:"+listContactBook[position].getmEmail());
+                editor.putStringSet("Contact_"+numContact, newSet);
+                editor.putInt("numContact", numContact);
+                editor.commit();
+
+                finish();
+            }
+        });
 
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                String url = "http://dlab.sit.kmutt.ac.th/el_launcher/getAccountbyUsername.php";
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("strUser", searchUser.getText().toString()));
+
+                String resultServer = Util.getHttpPost(url, params);
+
+                try {
+                    JSONArray lContact = new JSONArray(resultServer);
+                    cList = new String[lContact.length()];
+                    listContactBook = new ContactBook[lContact.length()];
+                    for(int i=0; i<lContact.length(); i++){
+                        cList[i]=lContact.getJSONObject(i).getString("Username")+" ("+lContact.getJSONObject(i).getString("FName")+")";
+                        listContactBook[i]=new ContactBook(null, lContact.getJSONObject(i).getString("Username"), lContact.getJSONObject(i).getString("FName"), lContact.getJSONObject(i).getString("LName"), lContact.getJSONObject(i).getString("PhoneNum"), lContact.getJSONObject(i).getString("Email"));
+                    }
+                    ArrayAdapter<String> cAdapter = new ArrayAdapter<String>(ContactFind.this, android.R.layout.simple_expandable_list_item_1, cList);
+                    listContact.setAdapter(cAdapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
--- app/src/main/java/com/launcher/elderlylauncher/ContactsActivity.java
+++ app/src/main/java/com/launcher/elderlylauncher/ContactsActivity.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by nicha on 9/13/16.
@@ -22,36 +24,76 @@ public class ContactsActivity extends Activity {
     private Button findContact;
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        refreshContact();
+    }
+
+    @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_contacts);
-
-        SharedPreferences sharedPreferences = getSharedPreferences("PREF_NAME", Context.MODE_PRIVATE);
-        final String AccountID = sharedPreferences.getString("AccountID", "");
 
         listView = (ListView)findViewById(R.id.listView1);
 
         findContact = (Button)findViewById(R.id.btnFindContact);
 
-        //Add Contact
-        List<ContactBook> listPhoneBook = new ArrayList<ContactBook>();
-        listPhoneBook.add(new ContactBook(BitmapFactory.decodeResource(getResources(), R.drawable.c_avatar0), "Daugther", "Varistha", "Thato", "098-765-4321", "daugther@gmail.com"));
-        listPhoneBook.add(new ContactBook(BitmapFactory.decodeResource(getResources(), R.drawable.c_avatar1), "Son", "Anyakrit", "Thato", "098-765-4321", "son@gmail.com"));
-        listPhoneBook.add(new ContactBook(BitmapFactory.decodeResource(getResources(), R.drawable.c_avatar2), "GrandDaughter", "Pahfun", "Thato", "098-765-4321", "grand_daugther@gmail.com"));
-        listPhoneBook.add(new ContactBook(BitmapFactory.decodeResource(getResources(), R.drawable.c_avatar3), "GrandSon", "Boonyarit", "Thato", "098-765-4321", "grand_son@gmail.com"));
-
-        ContactBookAdapter adapter = new ContactBookAdapter(this, listPhoneBook);
-        listView.setAdapter(adapter);
+        refreshContact();
 
         //Find People
         findContact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(ContactsActivity.this, ContactFind.class);
-                startActivity(i);
+                startActivityForResult(i,9999);
             }
         });
 
+    }
+
+    private void refreshContact() {
+        //Add Contact
+        List<ContactBook> listPhoneBook = new ArrayList<ContactBook>();
+//        listPhoneBook.add(new ContactBook(BitmapFactory.decodeResource(getResources(), R.drawable.c_avatar0), "Daugther", "Varistha", "Thato", "098-765-4321", "daugther@gmail.com"));
+//        listPhoneBook.add(new ContactBook(BitmapFactory.decodeResource(getResources(), R.drawable.c_avatar1), "Son", "Anyakrit", "Thato", "098-765-4321", "son@gmail.com"));
+//        listPhoneBook.add(new ContactBook(BitmapFactory.decodeResource(getResources(), R.drawable.c_avatar2), "GrandDaughter", "Pahfun", "Thato", "098-765-4321", "grand_daugther@gmail.com"));
+//        listPhoneBook.add(new ContactBook(BitmapFactory.decodeResource(getResources(), R.drawable.c_avatar3), "GrandSon", "Boonyarit", "Thato", "098-765-4321", "grand_son@gmail.com"));
+
+        SharedPreferences sharedPreferences = getSharedPreferences("PREF_NAME", Context.MODE_PRIVATE);
+        final String AccountID = sharedPreferences.getString("AccountID", "");
+
+        int count = sharedPreferences.getInt("numContact", 0);
+        for(int i=1; i<=count; i++){
+            Set<String> temp = sharedPreferences.getStringSet("Contact_"+i, null);
+            Log.d("temp", temp.toString());
+
+            String user = null;
+            String name = null;
+            String lname = null;
+            String phone = null;
+            String email = null;
+            for(String s : temp){
+                if(s==null){
+                    continue;
+                }
+                String[] val = s.split(":");
+                if(val[0].equals("user")){
+                    user = val[1];
+                }else if(val[0].equals("name")){
+                    name = val[1];
+                }else if(val[0].equals("lname")){
+                    lname = val[1];
+                }else if(val[0].equals("phone")){
+                    phone = val[1];
+                }else if(val[0].equals("email")){
+                    email = val[1];
+                }
+            }
+
+            listPhoneBook.add(new ContactBook(BitmapFactory.decodeResource(getResources(), R.drawable.c_avatar0), user, name, lname, phone, email));
+        }
+
+        ContactBookAdapter adapter = new ContactBookAdapter(this, listPhoneBook);
+        listView.setAdapter(adapter);
     }
 
 }
